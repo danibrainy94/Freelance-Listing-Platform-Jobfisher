c814cf679264fa504d4dd9985183a3042e6cfb88
danibrainy94 <danibrainy94@gmail.com>
2023-07-23 10:09:21 +0100

    main

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+        keyframes: {
+        slantIn: {
+          '0%': {'transform':'translateX(-800px)'},
+          '100%': {'transform':'translateX(0px)'},
+        },
+        rotate: {
+          '0%': {'transform':'rotate(90deg)'},
+          '100%': {'transform':'translateX(0px)'},
+        },
+        zoomIn: {
+          '0%': {'transform':'translateY(-100px)'},
+          '100%': {'transform':'translateY(0px)'},
+        },
+        zoom2: {
+          '0%': {'transform':'scale(1.2)'},
+          '100%': {'transform':'scale(1)'},
+        },
+      },
+      animation: {
+        'slantIn' : "slantIn .6s ease-in",
+        'zoomIn' : "zoomIn 1s ease-in",
+        'rotate': "rotate 2s ease-in",
+        'zoom2' : "zoom2 2s ease-in"
+      }
+    },
   },
   plugins: [],
 }

6646538f8e0c4f9005da53618ae371da6e1e1f5d
danibrainy94 <danibrainy94@gmail.com>
2023-07-24 09:51:45 +0100

    main

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,32 +3,39 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-        keyframes: {
+        fontFamily: {
+          'inter' : ['inter', 'sans-serif'],
+        },
+      keyframes: {
+        zoomIn: {
+          '0%': {'transform':'scale(1.1)'}, 
+          '100%': {'transform':'scale(1)'}, 
+        },
         slantIn: {
           '0%': {'transform':'translateX(-800px)'},
+          '100%': {'transform':'translateX(0px)'},
+        },
+        slant2: {
+          '0%': {'transform':'translateX(-100px)'},
           '100%': {'transform':'translateX(0px)'},
         },
         rotate: {
           '0%': {'transform':'rotate(90deg)'},
           '100%': {'transform':'translateX(0px)'},
         },
-        zoomIn: {
-          '0%': {'transform':'translateY(50px)'}, 
-          '100%': {'transform':'translateY(0px)'}, 
-        },
         zoom2: {
-          '0%': {'transform':'scale(1.2)blur(2px)'}, 
-          '100%': {'transform':'scale(1)blur(0px)'},
-          
+          '0%': {'transform':'scale(.7)'}, 
+          '100%': {'transform':'scale(1)'},
+        },
       },
       animation: {
         'slantIn' : "slantIn .6s ease-in",
+        'slant2' : "slant2 .6s ease-in",
         'zoomIn' : "zoomIn 3s ease-in",
         'rotate': "rotate 2s ease-in",
         'zoom2' : "zoom2 2s ease-in"
       }
     },
-  },
   plugins: [],
 }
 }
